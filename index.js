9cc43852bc5bf1e81a55fcdd7a01d9de20867c20
Stefan Nieuwenburg <82890714+Stefannieuwenburg@users.noreply.github.com>
2021-06-10 00:24:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// Stap 1
+const makePizza = function() {
+  console.log("Roll dough");
+  console.log("Spread tomato sauce");
+  console.log("Grate cheese");
+  console.log("Bake in oven");
+  console.log("eat nice pizza");
+};
+
+makePizza();
+
+// Stap 2
+const makeSushi = function() {
+  console.log("Boil rice");
+  console.log("Cut avocado and fish");
+  console.log("Roll sushi");
+  console.log("eat nice sushi");
+};
+makeSushi();
+
+
